221c5e23f29698a6b313601def87cdd0218b2780
Troy McNitt <troy.mcnitt@gmail.com>
2019-01-01 12:26:01 -0800

    Migrate to GitLab

---
+++ api/node.h
@@ -0,0 +1,27 @@
+typedef void GoNode;
+
+typedef struct NodeId {
+	uint32 homeId;
+	uint8 nodeId;
+} NodeId;
+
+typedef struct Node {
+	struct NodeId nodeId;
+	uint8 basicType;
+	uint8 genericType;
+	uint8 specificType;
+	char * nodeType;
+	char * manufacturerName;
+	char * productName;
+	char * nodeName;
+	char * location;
+	char * manufacturerId;
+	char * productType;
+	char * productId;
+} Node;
+
+extern void freeNode(Node *);
+extern void setNodeName(uint32 homeId, uint8 nodeId, char * name);
+#ifdef __cplusplus
+extern Node * exportNode(API * api, uint32 homeId, uint8 nodeId);
+#endif
---
+++ manager.cpp
@@ -0,0 +1,34 @@
+#include "api.h"
+
+// forwards the notification from the C++ API to the Go layer - caller must free.
+static void OnNotification (OpenZWave::Notification const* notification, API * api)
+{
+  Notification * exported = exportNotification(api, notification);
+  onNotificationWrapper(exported, api);
+}
+
+void startManager(API * api)
+{
+  OpenZWave::Manager::Create();
+  OpenZWave::Manager::Get()->AddWatcher( OnNotification, api );
+}
+
+void stopManager(API * api)
+{
+  OpenZWave::Manager::Get()->RemoveWatcher(OnNotification, api);
+  OpenZWave::Manager::Destroy();
+}
+
+bool addNode(uint32 homeId, bool secure) {
+  return OpenZWave::Manager::Get()->AddNode(homeId, secure);
+}
+
+bool addDriver(char * device)
+{
+  return OpenZWave::Manager::Get()->AddDriver(device);
+}
+
+bool removeDriver(char * device)
+{
+  return OpenZWave::Manager::Get()->RemoveDriver(device);
+}
